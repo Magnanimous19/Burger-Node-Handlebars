a2328d37642e166c34d745766e8b9bc5d15dd688
Reid B <reidburnsy19@hotmail.com>
2020-07-15 18:30:55 -0400

    more heroku

--- controllers/routes.js
+++ controllers/routes.js
@@ -6,8 +6,8 @@ router.get("/", function(req,res) {
     burger.all(function(burger_data) {
         console.log(burger_data);
         res.render("index", {burger_data});
-    })
-})
+    });
+});
 
 router.put("/burgers/update", function(req,res){
     burger.update(req.body.burger_id, function(result){
@@ -19,7 +19,7 @@ router.put("/burgers/update", function(req,res){
 router.post("/burgers/create", function(req,res) {
     burger.create(req.body.burger_name, function(result) {
         res.redirect("/");
-    })
-})
+    });
+});
 
 module.exports = router;
